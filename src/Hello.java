3aa8ff5349841eb8fce55d24e12c7cd0b60f8fb1
Mr-Hera <randellhera@gmail.com>
2023-06-26 00:01:56 +0300

    Feat: bonus challenge, add validation to  minutes & seconds method overload code challenge

--- src/Hello.java
+++ src/Hello.java
@@ -3,11 +3,18 @@ public class Hello {
     public static void main(String[] args) {
 
         System.out.println(getDurationString(3945));
+//        System.out.println(getDurationString(-65, 45));
+        System.out.println(getDurationString(65, 45));
+//        System.out.println(getDurationString(65, 145));
 
     }
 
     public static String getDurationString(int seconds) {
 //        1 minute = 60 seconds, 1 hour = 60 minutes || 3600 seconds
+
+        if(seconds < 0) {
+            return "Invalid input";
+        }
 
         int hours = seconds / 3600;
         int minutes = (seconds % 3600) / 60;
@@ -18,7 +25,14 @@ public class Hello {
 
     public static String getDurationString(int minutes, int seconds) {
 
-        return minutes + "m " + seconds + "s";
+
+        if(((minutes < 0) || (minutes > 59)) || ((seconds < 0) || (seconds > 59))) {
+            return "Invalid input";
+        }
+
+        String time = minutes + "m " + seconds + "s";
+
+        return time;
     }
 
 }
