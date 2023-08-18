ea53166b59901d40e7d230090f3761940b0fc7ab
Mr-Hera <randellhera@gmail.com>
2023-08-18 22:01:26 +0300

    Feat: while loop challenge exercise

--- src/Hello.java
+++ src/Hello.java
@@ -1,29 +1,41 @@
 public class Hello {
     public static void main(String[] args) {
 //        while Loop
-        for(int i = 1; i <= 5; i++) {
-            System.out.println(i);
-        }
+        int i = 4;
+        int evens = 0;
+        int odds = 0;
 
-        int j = 1;
-        boolean isReady = false;
-
-        do{
-            if(j>5) {
-                break;
+//        while(i <= 20) {
+//            i++;
+//            if(!isEvenNumber(i)) {
+//                odds++;
+//                continue;
+//            }
+//            evens++;
+//            System.out.println(i + " is an Even number!");
+//            if(evens >= 5) {
+//                break;
+//            }
+//        }
+        while(i <= 20) {
+            i++;
+            if (isEvenNumber(i) && evens < 5) {
+                evens++;
+                System.out.println(i + " is an Even number!");
+                if(!isEvenNumber(i+1)){
+                    System.out.println("Odd => " + (i + 1));
+                    odds++;
+                }
             }
-            System.out.println(j);
-            j++;
-            isReady = (j>0);
-        } while(isReady);
-
-        int num = 0;
-        while(num < 50) {
-            num += 5;
-            if(num % 25 == 0) {
-                continue;
-            }
-            System.out.print(num + "_");
         }
+        System.out.println("Total even nums = " + evens);
+        System.out.println("Total odd nums = " + odds);
+    }
+
+    public static boolean isEvenNumber(int num) {
+        if(num % 2 == 0) {
+            return true;
+        }
+        return false;
     }
 }
