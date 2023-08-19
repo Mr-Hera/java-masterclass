d874a478736dab0ed900fc8e22af966207836a60
Mr-Hera <randellhera@gmail.com>
2023-08-19 20:58:46 +0300

    Feat: Digit Sum Code CHallenge

--- src/Hello.java
+++ src/Hello.java
@@ -1,41 +1,23 @@
 public class Hello {
     public static void main(String[] args) {
 //        while Loop
-        int i = 4;
-        int evens = 0;
-        int odds = 0;
-
-//        while(i <= 20) {
-//            i++;
-//            if(!isEvenNumber(i)) {
-//                odds++;
-//                continue;
-//            }
-//            evens++;
-//            System.out.println(i + " is an Even number!");
-//            if(evens >= 5) {
-//                break;
-//            }
-//        }
-        while(i <= 20) {
-            i++;
-            if (isEvenNumber(i) && evens < 5) {
-                evens++;
-                System.out.println(i + " is an Even number!");
-                if(!isEvenNumber(i+1)){
-                    System.out.println("Odd => " + (i + 1));
-                    odds++;
-                }
-            }
-        }
-        System.out.println("Total even nums = " + evens);
-        System.out.println("Total odd nums = " + odds);
+        System.out.println("Sum of digits in 1234 => " + sumDigits(1234));
+        System.out.println("Sum of digits in -125 => " + sumDigits(-125));
+        System.out.println("Sum of digits in 4 => " + sumDigits(4));
+        System.out.println("Sum of digits in 32123 => " + sumDigits(32123));
     }
 
-    public static boolean isEvenNumber(int num) {
-        if(num % 2 == 0) {
-            return true;
+    public static int sumDigits(int number) {
+        int total = 0;
+        if(number < 0) {
+            return -1;
         }
-        return false;
+        while (number > 9) {
+            total += number % 10;
+            number = number / 10;
+        }
+
+        total += number;
+        return total;
     }
 }
