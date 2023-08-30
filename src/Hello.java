23fcb19c38b55439318a77eb5ad7f2d2b063a778
Mr-Hera <randellhera@gmail.com>
2023-08-31 01:55:14 +0300

    Feat: reading input with scanner

--- src/Hello.java
+++ src/Hello.java
@@ -1,10 +1,16 @@
+import java.util.Scanner;
+
 public class Hello {
     public static void main(String[] args) {
 //        while Loop
         int currentYear = 2023;
 
-        System.out.println(getInputFromConsole(currentYear));
-        System.out.println(getInputFromScanner(currentYear));
+
+        try {
+            System.out.println(getInputFromConsole(currentYear));
+        } catch(NullPointerException e) {
+            System.out.println(getInputFromScanner(currentYear));
+        }
     }
 
     public static String getInputFromConsole(int currentYear) {
@@ -17,7 +23,41 @@ public class Hello {
     }
 
     public static String getInputFromScanner(int currentYear) {
-        return "";
+        Scanner scanner = new Scanner(System.in);
+
+//        String name = System.console().readLine("Hi, what's your name?: ");
+        System.out.println("Hi, what's your name?: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Hi " + name + ", Thanks for taking the course!");
+        System.out.println("What year were you born? ");
+
+        boolean validDOB = false;
+        int age = 0;
+
+        do{
+            System.out.println("Enter year of birth >= " + (currentYear - 125) + " and <= " + (currentYear));
+            try {
+                age = checkData(currentYear, scanner.nextLine());
+                validDOB = age < 0 ? false : true;
+            } catch(NumberFormatException badUserData) {
+                System.out.println("Characters not allowed!!! Try again.");
+            };
+        } while(!validDOB);
+
+
+        return "So you're " + age + " years old...";
+    }
+
+    public static int checkData(int currentYear, String dateOfBirth) {
+        int dob = Integer.parseInt(dateOfBirth);
+        int minimumYear = currentYear - 125;
+
+        if((dob < minimumYear) || (dob > currentYear)) {
+            return -1;
+        }
+
+        return (currentYear - dob);
     }
 
 }
